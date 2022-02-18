6cf48bfd1ecb4127d507c8e12542749ea3430e6e
matined <nedzimateusz@gmail.com>
2022-02-18 12:12:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NLP Tweets Classification
+The aim is to predict whether a tweet is about a natural disaster or not using a dataset provided by a kaggle task.
+
+The task: https://www.kaggle.com/c/nlp-getting-started
---
+++ utils.py
@@ -0,0 +1,60 @@
+import numpy as np
+import re  # regex - regular expressions
+import pickle
+
+
+def process_tweet(tweet: str) -> np.ndarray:
+    """
+    Preprocess the tweet to lowercase, get rid of punctuation marks and replace:
+
+        URLs -> httpaddr
+
+        numbers -> numberrep 
+
+        @ -> tagrep 
+
+    Parameters
+    ----------
+    tweet : string
+        tweet to preprocess
+
+    Returns
+    -------
+    result : ndarray
+        A numpy ndarray of words
+    """
+
+    result = tweet
+
+    # lower case
+    result = result.lower()
+
+    # replace URLs with ' httpaddr '
+    result = re.compile('(http|https)://[^\s]*').sub('', result)
+
+    # replace numbers with ' number '
+    result = re.compile('[0-9]+').sub('', result)
+
+    # replacing tags with ' tagrep '
+    result = re.compile('[@]+').sub('', result)
+
+    # get rid of punctuation
+    result = re.split('[ @$/#.-:&*+=\[\]?!(){},''">_<;%\n\r]', result)
+
+    # remove empty strings
+    result = [word for word in result if len(word) > 0]
+
+    return np.array(result)
+
+
+def load_dictionary() -> tuple:
+    word_to_vector = pickle.load(open('../data/dictionary/word_to_vector.pkl', 'rb'))
+    words_to_index = pickle.load(open('../data/dictionary/words_to_index.pkl', 'rb'))
+    index_to_words = pickle.load(open('../data/dictionary/index_to_words.pkl', 'rb'))
+    return word_to_vector, words_to_index, index_to_words
+
+
+def load_training_input() -> tuple:
+    training_input = pickle.load(open('../data/training_input.pkl', 'rb'))
+    X, y = training_input['X'], training_input['y']
+    return X, y
